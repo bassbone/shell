eb4388dd2878f20c0f605db3ef6a053f40ac9c27
bassbone <bassbone0o_o0@yahoo.co.jp>
2016-07-08 22:45:39 +0000

    apache_tally

--- apache_tally.sh
+++ apache_tally.sh
@@ -16,6 +16,10 @@ elif [ "$2" = "-a" ]; then # per IP address
   exit 0
 elif [ "$2" = "-u" ]; then # per URI
   cat $1|awk '{print $7}'|sort|uniq -c|sort -nr
+elif [ "$2" = "-u1" ]; then # per URI 1st hierarchy
+  cat $1|awk '{print $7}'|sed -E "s/(\/.*?)\/.*$/\1/"|sort|uniq -c|sort -nr
+elif [ "$2" = "-u2" ]; then # per URI 2nd hierarchy
+  cat $1|awk '{print $7}'|sed -E "s/(\/.*?)(\/.*?)\/.*$/\1\2/g"|sed -E "s/(\/.*)\/$/\1/g"|sort|uniq -c|sort -nr
 else
   echo "usage: $0 [access_log] [-hau]"
   exit 0
