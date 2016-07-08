3ffe1a870098b60df746317da6a155ffd891da5b
bassbone <bassbone0o_o0@yahoo.co.jp>
2016-07-08 14:30:34 +0000

    apache_tally

--- apache_tally.sh
+++ apache_tally.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
 if [ "$1" = "" ]; then
-  echo "usage: $0 [access_log] [-h]"
+  echo "usage: $0 [access_log] [-hau]"
   exit 0
 elif [ ! -f "$1" ]; then
   echo "$1 is not found."
   exit 1
 fi
 
-if [ "$2" = "-h" ]; then
-  cat $1|awk '{print $4}'|cut -c14-15|sort|uniq -c
+if [ "$2" = "-h" ]; then # per hour
+  cat $1|awk '{print $4}'|cut -c14-15|awk '{print $1 "時"}'|sort|uniq -c
   exit 0
-elif [ "$2" = "-a" ]; then
-  cat $1|awk '{print $1}'|sort|uniq -c|sort -r
+elif [ "$2" = "-a" ]; then # per IP address 
+  cat $1|awk '{print $1}'|sort|uniq -c|sort -nr
   exit 0
+elif [ "$2" = "-u" ]; then # per URI
+  cat $1|awk '{print $7}'|sort|uniq -c|sort -nr
 else
-  echo "usage: $0 [access_log] [-h]"
+  echo "usage: $0 [access_log] [-hau]"
   exit 0
 fi
 
