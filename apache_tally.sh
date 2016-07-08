061793a8b28cb32ed1c346c0b7494f67d5b5d247
bassbone <bassbone0o_o0@yahoo.co.jp>
2016-07-08 05:33:26 +0000

    apache_tally

--- apache_tally.sh
+++ apache_tally.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-cat $1|awk '{print $4}'|cut -c14-15|sort|uniq -c
+
+if [ "$2" = "-h" ]; then
+  cat $1|awk '{print $4}'|cut -c14-15|sort|uniq -c
+else
+  echo "usage: $0 [access_log] [-h]"
+fi
 
 exit
 
