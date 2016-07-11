53ba9caf060ecc810627b5bd78b7346ea58d12e3
bassbone <bassbone0o_o0@yahoo.co.jp>
2016-07-11 14:51:51 +0900

    apache_tally

--- apache_tally.sh
+++ apache_tally.sh
@@ -17,9 +17,9 @@ elif [ "$2" = "-a" ]; then # per IP address
 elif [ "$2" = "-u" ]; then # per URI
   cat $1|awk '{print $7}'|sort|uniq -c|sort -nr
 elif [ "$2" = "-u1" ]; then # per URI 1st hierarchy
-  cat $1|awk '{print $7}'|sed -E "s/(\/.*?)\/.*$/\1/"|sort|uniq -c|sort -nr
+  cat $1|awk '{print $7}'|sed -r "s/(\/.*?)\/.*$/\1/"|sort|uniq -c|sort -nr
 elif [ "$2" = "-u2" ]; then # per URI 2nd hierarchy
-  cat $1|awk '{print $7}'|sed -E "s/(\/.*?)(\/.*?)\/.*$/\1\2/g"|sed -E "s/(\/.*)\/$/\1/g"|sort|uniq -c|sort -nr
+  cat $1|awk '{print $7}'|sed -r "s/(\/.*?)(\/.*?)\/.*$/\1\2/g"|sed -E "s/(\/.*)\/$/\1/g"|sort|uniq -c|sort -nr
 elif [ "$2" = "-s" ]; then # per status code
   cat $1|awk '{print $9}'|sort|uniq -c
 else
