2271e0ae33e106753acfe7a5c5b3ccbdad88c4bc
bassbone <bassbone0o_o0@yahoo.co.jp>
2016-07-11 09:10:27 +0900

    security_check

--- security_check.sh
+++ security_check.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+echo "*** nmap ***"
 nmap localhost
+echo ""
 
+echo "*** lastlog ***"
+lastlog|grep -v "Never"
+echo ""
+
+echo "*** yum update check ***"
+yum check-update
+echo ""
 
