2b66180d458807a910bcbd04b15038595d8a9e34
bassbone <bassbone0o_o0@yahoo.co.jp>
2016-07-12 15:11:18 +0900

    security_check

--- security_check.sh
+++ security_check.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$1" == "" ]; then
+  echo "usage: $0 [self IP address]"
+  exit 1
+fi
+
 echo "*** nmap ***"
 nmap localhost
 echo ""
@@ -9,10 +14,13 @@ lastlog|grep -v "Never"
 echo ""
 
 echo "*** last ***"
-last|tail -10
+last|grep -v "system boot"|grep -v "$1"
 echo ""
 
 echo "*** yum update check ***"
 yum check-update
 echo ""
 
+echo "*** iptables ***"
+iptables -L
+echo ""
